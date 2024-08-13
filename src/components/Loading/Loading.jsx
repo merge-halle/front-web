0370e777413b9e8e5bc286b7dbc62cb4d3897d33
NanaManBo <kimbosung22@naver.com>
2024-08-14 03:21:05 +0900

    모달창 수정

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,35 +1,46 @@
-import React, { useState, useEffect } from "react";
-import RobotImage from "../../assets/robot.png";
-import "./Loading.scss";
-import { Button, Modal } from "antd";
+import React, { useState, useEffect } from 'react';
+import RobotImage from '../../assets/robot.png';
+import './Loading.scss';
+import { Button, Modal } from 'antd';
 
 export default function Loading({ text }) {
-	const [dots, setDots] = useState("");
+  const [dots, setDots] = useState('');
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setDots((prevDots) => (prevDots.length < 3 ? prevDots + "." : ""));
-		}, 500); // 0.5초마다 점을 추가하거나 초기화
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) =>
+        prevDots.length < 3 ? prevDots + '.' : ''
+      );
+    }, 500); // 0.5초마다 점을 추가하거나 초기화
 
-		return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
-	}, []);
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
+  }, []);
 
-	return (
-		<div>
-			<Modal
-				open={true}
-				footer={null}
-				closable={false}
-				className="loading-container"
-			>
-				<div style={{ width: "150px", display:"flex", flexDirection:'column' ,justifyContent:'center', alignContent:'center'}}>
-					<img src={RobotImage} alt="robotImage" />
-					<p>
-						{text}
-						{dots}
-					</p>
-				</div>
-			</Modal>
-		</div>
-	);
+  return (
+    <div className="loading-page">
+      <Modal
+        open={true}
+        footer={null}
+        closable={false}
+        className="loading-container"
+      >
+        <div
+          style={{
+            width: '200px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            alignContent: 'center',
+          }}
+        >
+          <img src={RobotImage} alt="robotImage" />
+          <p>
+            {text}
+            {dots}
+          </p>
+        </div>
+      </Modal>
+    </div>
+  );
 }
--- src/pages/TestProductReview/TestProductReview.jsx
+++ src/pages/TestProductReview/TestProductReview.jsx
@@ -1,93 +1,94 @@
-import React, { useState } from "react";
-import { Input, Button } from "antd";
-import "./TestProductReview.scss";
-import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
-import Loading from "../../components/Loading/Loading";
+import React, { useState } from 'react';
+import { Input, Button } from 'antd';
+import './TestProductReview.scss';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import Loading from '../../components/Loading/Loading';
 
 const { TextArea } = Input;
 
 export default function TestProductReview() {
-	const [useAI, setUseAI] = useState(false);
-	const [isLoading, setIsLoading] = useState(false);
-	const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  const [useAI, setUseAI] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
-	const handleClick = () => {
-		setUseAI(!useAI);
-	};
+  const handleClick = () => {
+    setUseAI(!useAI);
+  };
 
-	// ai로 리뷰 받아오기
-	const handleReview = () => {
-		// getReviewMutation.mutate();
-		setIsLoading(true);
-	};
+  // ai로 리뷰 받아오기
+  const handleReview = () => {
+    // getReviewMutation.mutate();
+    setIsLoading(true);
+  };
 
-	// api
-	const getReview = async () => {
-		const response = await axios.post(`${baseUrl}/`, {});
-		return response;
-	};
+  // api
+  const getReview = async () => {
+    const response = await axios.post(`${baseUrl}/`, {});
+    return response;
+  };
 
-	// api Mutation
-	const getReviewMutation = useMutation({
-		mutationFn: getReview,
-		onSuccess: (response) => {
-			setIsLoading(false);
-		},
-		onError: (error) => {
-			console.error("sign up failed", error);
-		},
-	});
+  // api Mutation
+  const getReviewMutation = useMutation({
+    mutationFn: getReview,
+    onSuccess: (response) => {
+      setIsLoading(false);
+    },
+    onError: (error) => {
+      console.error('sign up failed', error);
+    },
+  });
 
-	return (
-		<div className="page">
-			{useAI ? (
-				<div>
-					<Button
-						type="primary"
-						onClick={handleClick}
-						className="button"
-					>
-						AI 없이 리뷰 작성하기
-					</Button>
-					<h3>사이즈 및 핏</h3>
-					<Input placeholder="사이즈 및 핏에 대해 입력해주세요." />
-					<h3>품질 및 내구성</h3>
-					<Input placeholder="품질 및 내구성에 대해 입력해주세요." />
-					<h3>가성비</h3>
-					<Input placeholder="가성비에 대해 입력해주세요." />
-					<h3>재구매 의사</h3>
-					<Input placeholder="재구매 의사에 대해 입력해주세요." />
-					<Button
-						type="primary"
-						onClick={handleReview}
-						className="button"
-					>
-						리뷰 생성
-					</Button>
-				</div>
-			) : (
-				<div>
-					<Button
-						type="primary"
-						onClick={handleClick}
-						className="button"
-					>
-						AI 사용해서 리뷰 작성하기
-					</Button>
-					<TextArea rows={10} />
-					<Button type="primary" className="button">
-						리뷰 등록하기
-					</Button>
-				</div>
-			)}
-			{isLoading && (
-				<div className="overlay">
-				</div>
-			)}
-      {isLoading && (
-				<Loading text={"리뷰를 생성중입니다"}/>
-			)}
-		</div>
-	);
+  return (
+    <>
+      {isLoading && useAI && (
+        <>
+          <div className="overlay"></div>
+          <Loading text={'리뷰를 생성중입니다'} />
+        </>
+      )}
+      <div className="page">
+        {useAI ? (
+          <div>
+            <Button
+              type="primary"
+              onClick={handleClick}
+              className="button"
+            >
+              AI 없이 리뷰 작성하기
+            </Button>
+            <h3>사이즈 및 핏</h3>
+            <Input placeholder="사이즈 및 핏에 대해 입력해주세요." />
+            <h3>품질 및 내구성</h3>
+            <Input placeholder="품질 및 내구성에 대해 입력해주세요." />
+            <h3>가성비</h3>
+            <Input placeholder="가성비에 대해 입력해주세요." />
+            <h3>재구매 의사</h3>
+            <Input placeholder="재구매 의사에 대해 입력해주세요." />
+            <Button
+              type="primary"
+              onClick={handleReview}
+              className="button"
+            >
+              리뷰 생성
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button
+              type="primary"
+              onClick={handleClick}
+              className="button"
+            >
+              AI 사용해서 리뷰 작성하기
+            </Button>
+            <TextArea rows={10} />
+            <Button type="primary" className="button">
+              리뷰 등록하기
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
