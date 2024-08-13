f1d4d746ce922f7cde10b60e2a5b22d33994fd96
NanaManBo <kimbosung22@naver.com>
2024-08-13 15:28:16 +0900

    🐛

--- src/pages/Login/Loginpage.jsx
+++ src/pages/Login/Loginpage.jsx
@@ -2,46 +2,5 @@ import React, { useState } from 'react';
 import './Loginpage.scss';
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // 여기에 로그인 로직을 추가합니다.
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
-
-  return (
-    <div className="login-page">
-      <div className="login-container">
-        <h2>로그인</h2>
-        <form onSubmit={handleLogin}>
-          <div className="input-group">
-            <label htmlFor="email">ID</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">PW</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="login-button">
-            Go!
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+  return <div className="login-page"></div>;
 }
