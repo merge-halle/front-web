b5a0d247b37b245ed465f296c44940d40d2eb610
NanaManBo <kimbosung22@naver.com>
2024-08-14 03:28:32 +0900

    모달창 수정 후 기업 페이지에 적용

--- src/pages/ProductInfoInput/ProductInfoInput.jsx
+++ src/pages/ProductInfoInput/ProductInfoInput.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import './ProductInfoInput.scss';
 import { Input, Button, Modal, message } from 'antd';
 import axios from 'axios';
+import Loading from '../../components/Loading/Loading';
 
 const { TextArea } = Input;
 
 export default function ProductInfoInput() {
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태를 저장할 state
   const [productInfo, setProductInfo] = useState(''); // TextArea 값을 담을 state
   const [previewData, setPreviewData] = useState([
     '디자인',
@@ -21,6 +23,7 @@ export default function ProductInfoInput() {
   };
 
   const handleSubmit = async () => {
+    setIsLoading(true); // 요청을 보내기 전 로딩 상태로 변경
     try {
       // const response = await axios.post(
       //   'https://your-server-endpoint.com/api',
@@ -28,11 +31,16 @@ export default function ProductInfoInput() {
       //     productInfo, // 서버로 보낼 데이터
       //   }
       // );
-      message.success('AI 요청 완료!'); // 요청 성공 시 메시지 출력
+
       // 서버에서 받은 데이터를 배열로 저장
       //setPreviewData(response.data);
       // 서버 응답이 성공적이면 미리보기 버튼 보이도록 설정
-      setIsPreviewAvailable(true);
+      setTimeout(() => {
+        // 3초 뒤에 실행
+        message.success('AI 요청 완료!'); // 요청 성공 시 메시지 출력
+        setIsLoading(false);
+        setIsPreviewAvailable(true);
+      }, 3000);
     } catch (error) {
       console.error('서버 요청 에러:', error); // 요청 중 에러 발생 시 콘솔에 에러 출력
     }
@@ -47,59 +55,67 @@ export default function ProductInfoInput() {
   };
 
   return (
-    <div className="page">
-      <h3>제품 설명</h3>
-      <TextArea
-        rows={10}
-        value={productInfo}
-        onChange={handleTextChange}
-      />
-      <div className="buttons">
-        <Button
-          type="primary"
-          className="button"
-          onClick={handleSubmit}
-        >
-          AI 요청
-        </Button>
-        {isPreviewAvailable && (
+    <>
+      {isLoading && (
+        <>
+          <div className="overlay"></div>
+          <Loading text={'후기 폼을 생성중입니다'} />
+        </>
+      )}
+      <div className="page">
+        <h3>제품 설명</h3>
+        <TextArea
+          rows={10}
+          value={productInfo}
+          onChange={handleTextChange}
+        />
+        <div className="buttons">
           <Button
-            type="default"
+            type="primary"
             className="button"
-            onClick={handlePreview}
+            onClick={handleSubmit}
           >
-            미리보기
+            AI 요청
           </Button>
-        )}
+          {isPreviewAvailable && (
+            <Button
+              type="default"
+              className="button"
+              onClick={handlePreview}
+            >
+              미리보기
+            </Button>
+          )}
+        </div>
+        <Modal
+          title={
+            <h2
+              style={{
+                fontSize: '24px',
+                fontWeight: 'bold',
+                marginBottom: 40,
+              }}
+            >
+              미리보기
+            </h2>
+          }
+          visible={isModalVisible}
+          onCancel={handleModalClose}
+          closable={false} // X 버튼을 제거하려면 이 속성을 추가
+          footer={[
+            <Button key="close" onClick={handleModalClose}>
+              닫기
+            </Button>,
+          ]}
+        >
+          {previewData.map((item, index) => (
+            <div key={index} className="preview-item">
+              <p className="pre-title">{item}</p>
+              <Input className="pre-input" />
+            </div>
+          ))}
+        </Modal>
       </div>
-      <Modal
-        title={
-          <h2
-            style={{
-              fontSize: '24px',
-              fontWeight: 'bold',
-              marginBottom: 40,
-            }}
-          >
-            미리보기
-          </h2>
-        }
-        visible={isModalVisible}
-        onCancel={handleModalClose}
-        closable={false} // X 버튼을 제거하려면 이 속성을 추가
-        footer={[
-          <Button key="close" onClick={handleModalClose}>
-            닫기
-          </Button>,
-        ]}
-      >
-        {previewData.map((item, index) => (
-          <div key={index} className="preview-item">
-            <p className="pre-title">{item}</p>
-            <Input className="pre-input" />
-          </div>
-        ))}
-      </Modal>
-    </div>
+    </>
   );
 }
