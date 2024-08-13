dda8092de18162f86842395e8f94c8b3f7c73b26
NanaManBo <kimbosung22@naver.com>
2024-08-13 14:36:30 +0900

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import './Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
 export default function Header() {
-  return <div className="main-header"></div>;
+  return (
+    <div className="main-header">
+      <p className="header-logo">Header</p>
+      <button className="header-button">
+        <FontAwesomeIcon
+          icon={faRightToBracket}
+          style={{ marginRight: 5 }}
+        />
+        로그인
+      </button>
+    </div>
+  );
 }
