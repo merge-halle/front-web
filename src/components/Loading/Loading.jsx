5e8ae512e923cb39741e6a8f8b3c2fc17a09f077
SanghyunLee <lgw9736@naver.com>
2024-08-14 03:00:23 +0900

    🐛

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import RobotImage from "../../assets/robot.png";
+import "./Loading.scss";
+import { Button, Modal } from "antd";
+
+export default function Loading({ text }) {
+	const [dots, setDots] = useState("");
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDots((prevDots) => (prevDots.length < 3 ? prevDots + "." : ""));
+		}, 500); // 0.5초마다 점을 추가하거나 초기화
+
+		return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
+	}, []);
+
+	return (
+		<div>
+			<Modal
+				open={true}
+				footer={null}
+				closable={false}
+				className="loading-container"
+			>
+				<div style={{ width: "150px", display:"flex", flexDirection:'column' ,justifyContent:'center', alignContent:'center'}}>
+					<img src={RobotImage} alt="robotImage" />
+					<p>
+						{text}
+						{dots}
+					</p>
+				</div>
+			</Modal>
+		</div>
+	);
+}
--- src/pages/TestProductReview/TestProductReview.jsx
+++ src/pages/TestProductReview/TestProductReview.jsx
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
 import { Input, Button } from "antd";
 import "./TestProductReview.scss";
-import { useMutation} from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import Loading from "../../components/Loading/Loading";
 
 const { TextArea } = Input;
 
 export default function TestProductReview() {
 	const [useAI, setUseAI] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 	const handleClick = () => {
 		setUseAI(!useAI);
 	};
 
-  const getReview = async () => {
+	// ai로 리뷰 받아오기
+	const handleReview = () => {
+		// getReviewMutation.mutate();
+		setIsLoading(true);
+	};
 
-  }
+	// api
+	const getReview = async () => {
+		const response = await axios.post(`${baseUrl}/`, {});
+		return response;
+	};
+
+	// api Mutation
+	const getReviewMutation = useMutation({
+		mutationFn: getReview,
+		onSuccess: (response) => {
+			setIsLoading(false);
+		},
+		onError: (error) => {
+			console.error("sign up failed", error);
+		},
+	});
 
 	return (
 		<div className="page">
@@ -35,27 +58,36 @@ export default function TestProductReview() {
 					<Input placeholder="가성비에 대해 입력해주세요." />
 					<h3>재구매 의사</h3>
 					<Input placeholder="재구매 의사에 대해 입력해주세요." />
+					<Button
+						type="primary"
+						onClick={handleReview}
+						className="button"
+					>
+						리뷰 생성
+					</Button>
 				</div>
 			) : (
-        <div>
-          <Button
+				<div>
+					<Button
 						type="primary"
 						onClick={handleClick}
 						className="button"
 					>
 						AI 사용해서 리뷰 작성하기
 					</Button>
-          <TextArea rows={10} />
-        </div>
-				
-			)}
-      <Button
-						type="primary"
-						onClick={handleClick}
-						className="button"
-					>
+					<TextArea rows={10} />
+					<Button type="primary" className="button">
 						리뷰 등록하기
 					</Button>
+				</div>
+			)}
+			{isLoading && (
+				<div className="overlay">
+				</div>
+			)}
+      {isLoading && (
+				<Loading text={"리뷰를 생성중입니다"}/>
+			)}
 		</div>
 	);
 }
