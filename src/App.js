01d2f8f5444b8f83c2966f085dd2cd295ee8a2be
NanaManBo <kimbosung22@naver.com>
2024-08-13 14:07:26 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import "./App.css";
-import { Outlet, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
+import './App.css';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const Layout = () => (
-	<div className="wrap">
-		<Header />
-		<Outlet />
-	</div>
+  <>
+    <Header />
+    <div className="wrap">
+      <Outlet />
+    </div>
+  </>
 );
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" />
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" />
           <Route path="/login" />
-				</Route>
-			</Routes>
-		</>
-	);
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
