edb97d75b209d607aecb2c2269adb9b6c48e7fb3
NanaManBo <kimbosung22@naver.com>
2024-08-13 15:59:00 +0900

    🐛

--- src/pages/Main/Mainpage.jsx
+++ src/pages/Main/Mainpage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Mainpage.scss';
-import Header from '../../components/Header/Header';
 
 export default function Mainpage() {
   return <div className="main-page"></div>;
